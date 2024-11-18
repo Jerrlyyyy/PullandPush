708b1c23f4d58667f6aadff5e740d225d8e055db
Chris-G <09xghagox09@gmail.com>
2024-11-18 14:33:14 +0800

    gago ka

--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './RegistrationForm.css';
-
+//gago ka
 const RegistrationForm = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
